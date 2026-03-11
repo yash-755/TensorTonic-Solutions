e1ba44d0aed9d8ce21c403e384450933bf5f8612
Yash Uttam <ur731084@gmail.com>
2026-03-11 23:32:26 +0530

    Update solution for dot-product

--- dot-product/dot-product.py
+++ dot-product/dot-product.py
@@ -11,6 +11,6 @@ def dot_product(x, y):
 
     if x.shape != y.shape:
         raise ValueError("Vectors must have the same length")
-        
+             
     return float(np.dot(x, y)) 
     pass
