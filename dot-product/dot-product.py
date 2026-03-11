20156fc18489554452fd0c91bfafef5e8cd9104f
Yash Uttam <ur731084@gmail.com>
2026-03-11 23:29:45 +0530

    Add solution for dot-product

---
+++ dot-product/dot-product.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def dot_product(x, y):
+    """
+    Compute the dot product of two 1D arrays x and y.
+    Must return a float.
+    """
+    # Write code here
+    x = np.array(x, dtype=float)
+    y = np.array(y, dtype=float)
+
+    if x.shape != y.shape:
+        raise ValueError("Vectors must have the same length")
+
+    return float(np.dot(x, y))
+    pass
